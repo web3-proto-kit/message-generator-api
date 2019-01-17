1263b85fd23a09c2f1edea5e028a3e69253e3c38
Cael Rowley <caelrowley@gmail.com>
2019-01-17 20:44:17 +0000

    Fixed all linting errors

--- utility/create-uuid.js
+++ utility/create-uuid.js
@@ -1,8 +1,11 @@
-function uuidv4() {
-   return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-     var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
-     return v.toString(16);
-   });
- }
+/* eslint-disable no-bitwise, no-mixed-operators, no-undef, eqeqeq */
 
- module.exports = { "uuidv4": uuidv4 }
+function uuidv4() {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
+    const r = Math.random() * 16 | 0; const
+      v = c == 'x' ? r : (r & 0x3 | 0x8);
+    return v.toString(16);
+  });
+}
+
+module.exports = { uuidv4 };
